316fa8b3bee1824b4514d33da3893069bb91790d
Milaux <hamchaouiselyan@gmail.com>
2022-12-14 15:02:52 +0100

    ajout des variables de la db 3

--- app/src/main/java/com/example/dart/Statistiques.java
+++ app/src/main/java/com/example/dart/Statistiques.java
@@ -132,7 +132,7 @@ public class Statistiques extends AppCompatActivity {
     }
 
     public void Affichage_Stats(){ //fonction qui permet de mettre nos stats de la db avec nos textes
-        TV1.setText("\n\n\nMeilleur lancer : " + strMeilleurLanceFlechette + "\n\nNombre d'amis : " + strnbAmis + "\n\nNombre de sets gagnés : " + strnbSetGagnes + "\n\nNombre de Legs gagnés : " + strnbLegGagnes + "\n\nDernier meilleur lancé : " + strPseudoJoueurs );
+        TV1.setText("\n\n\n\nMeilleur lancer : " + strMeilleurLanceFlechette + "\n\nNombre d'amis : " + strnbAmis + "\n\nNombre de sets gagnés : " + strnbSetGagnes + "\n\nNombre de Legs gagnés : " + strnbLegGagnes + "\n\nDernier meilleur lancé : " + strPseudoJoueurs );
     }
 
 
